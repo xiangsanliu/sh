b21365b5520ea49b7fecf9675d3a382321f1e203
Chien <xiangjianjian@hotmail.com>
2023-02-25 12:00:55 +0800

    add post_install_zsh.sh

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sh
+
+个人使用的小shell。
+
+
+## post_install_zsh.sh
+
+安装，oh-my-zsh。
+
+```shell
+zsh <(curl -fsSL https://ghproxy.com/raw.githubusercontent.com/xiangsanliu/sh/master/post_install_zsh.sh)
+```
---
+++ post_install_zsh.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+mv ~/.zshrc ~/.zshrc.bak
+wget https://ghproxy.com/raw.githubusercontent.com/zsh-users/antigen/master/bin/antigen.zsh -O ~/.antigen.zsh
+cat >~/.zshrc<<EOF # 开始
+source ~/.antigen.zsh 
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-history-substring-search
+
+# Load the theme.
+antigen theme ys
+
+# Tell Antigen that you're done.
+antigen apply
+
+if [[ -f ~/.zprofile ]]; then
+  source ~/.zprofile
+fi
+EOF
+echo 'Please input password.'
+chsh -s $(which zsh)
+zsh
